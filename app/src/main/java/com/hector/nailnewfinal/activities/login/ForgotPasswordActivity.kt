cc90dfacdba02bcbe9ecca1e2ed4f84282937a7d
Hunter64 <46069666+hunter64@users.noreply.github.com>
2019-08-14 14:33:07 -0500

    Function for forgot password

--- app/src/main/java/com/hector/nailnewfinal/activities/login/ForgotPasswordActivity.kt
+++ app/src/main/java/com/hector/nailnewfinal/activities/login/ForgotPasswordActivity.kt
@@ -1,20 +1,48 @@
 package com.hector.nailnewfinal.activities.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.hector.nailnewfinal.R
 import com.hector.nailnewfinal.activities.extensions.goToActivity
+import com.hector.nailnewfinal.activities.extensions.isValidEmail
+import com.hector.nailnewfinal.activities.extensions.toast
+import com.hector.nailnewfinal.activities.extensions.validate
 import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
 
+        editTextEmail.validate { editTextEmail.error = if(isValidEmail(it)) null else "Email isn't valid!" }
+
         buttonGoLogIn.setOnClickListener {
-            goToActivity<LoginActivity>()
+            goToActivity<LoginActivity>{
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
         }
+
+        buttonForgot.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            if(isValidEmail(email)){
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(this){
+                    toast("Email has been send to reset your password")
+                    goToActivity<LoginActivity>{
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    }
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                }
+            }
+            else{
+                toast("Please make sure that email is correct!")
+            }
+        }
+
     }
 }
