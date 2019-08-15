68c770f0aa766e3885d7ba210955b4e2266f6bbc
Hunter64 <46069666+hunter64@users.noreply.github.com>
2019-08-15 15:59:57 -0500

    Working PagerAdapter including layout for this and add implementation for this in build gradle app

--- app/src/main/java/com/hector/nailnewfinal/activities/MainActivity.kt
+++ app/src/main/java/com/hector/nailnewfinal/activities/MainActivity.kt
@@ -1,20 +1,63 @@
 package com.hector.nailnewfinal.activities
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import androidx.viewpager.widget.ViewPager
 import com.google.firebase.auth.FirebaseAuth
 import com.hector.mylibrary.ToolbarActivity
 import com.hector.nailnewfinal.R
+import com.hector.nailnewfinal.adapters.PagerAdapter
+import com.hector.nailnewfinal.fragments.ChatFragment
+import com.hector.nailnewfinal.fragments.InfoFragment
+import com.hector.nailnewfinal.fragments.RatesFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : ToolbarActivity() {
 
     private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
+    private lateinit var adapter: PagerAdapter
+
+    private var prevBottomSelected: MenuItem? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         toolbarToLoad(toolbarView as Toolbar)
+    }
+
+    private fun getPagerAdapter(): PagerAdapter{
+        adapter = PagerAdapter(supportFragmentManager)
+        adapter.addFragment(ChatFragment())
+        adapter.addFragment(InfoFragment())
+        adapter.addFragment(RatesFragment())
+        return adapter
+    }
+
+    private fun setUpViewPager(adapter: PagerAdapter){
+        viewPager.adapter = adapter
+        viewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(state: Int) {}
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+            override fun onPageSelected(position: Int) {
+                if(prevBottomSelected == null){
+                    bottomNavigation.menu.getItem(0).isChecked = false
+                }
+                else{
+                    prevBottomSelected!!.isChecked = false
+                }
+                bottomNavigation.menu.getItem(position).isChecked = true
+                prevBottomSelected = bottomNavigation.menu.getItem(position)
+            }
+
+        })
+    }
+
+    private fun setUpBottomNavigationBar(){
+
     }
 }
